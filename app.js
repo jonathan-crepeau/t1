259310f0c225b3c502c737e9eaa9cf35764ea0d2
jonathan-crepeau <jon.crepeau@gmail.com>
2024-02-02 18:16:09 -0800

    for loop exercises completed

--- app.js
+++ app.js
@@ -25,3 +25,30 @@
 //   console.log('The number is ' + num);
 //   num--;
 // }
+
+
+// ANCHOR - For Loop Exercises
+
+// SECTION - Activity #1: Write a for loop that will count from 0 to 99.
+
+// for (let i = 0; i < 100; i++) {
+//   console.log('The number is: ' + i);
+// }
+
+// SECTION - Activity #2: Write a for loop that counts from 999 to 9999.
+
+// for (let i = 999; i < 10000; i++) {
+//   console.log('The number is: ' + i);
+// }
+
+// SECTION - Activity #3: Write a loop that prints 'this is a song that never ends to the console 100 times.
+
+// for (let i = 0; i < 100; i++) {
+//   console.log('This is a song that never ends');
+// }
+
+// SECTION - Activity #4: Write a for loop that counts backwards from 1000 to 1, using the postfix operator i--.
+
+// for (let i = 1000; i > 0; i--) {
+//   console.log('The number is: ' + i);
+// }
