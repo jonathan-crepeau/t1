70cb83817c9bcdecc58a8f456219d3d575853a55
jonathan-crepeau <jon.crepeau@gmail.com>
2024-02-02 17:21:41 -0800

    while loop exercises complete

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('Fear is the mind killer.');
+// console.log('Fear is the mind killer.');
+
+//ANCHOR - While Loop Exercises
+
+// SECTION - Activity #1: Write a while loop that counts from 1 to 100:
+
+// let num = 1;
+// while (num < 101) {
+//   console.log('The number is: ' + num);
+//   num++;
+// }
+
+// SECTION - Activity #2: Write another while loop, but write it from from memory. Make the loop count from 0 to 5000 and print the square of each number.
+
+// let num = 0;
+// while (num < 5001) {
+//   console.log('The square of ' + num + ' is ' + (num**2));
+//   num++;
+// }
+
+// SECTION - Activity #3: Write a while loop that counts backwards from 1000 to 1.
+
+// let num = 1000;
+// while (num > 0) {
+//   console.log('The number is ' + num);
+//   num--;
+// }
